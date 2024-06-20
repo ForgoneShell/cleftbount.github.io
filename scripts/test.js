2fb6e8414749dbf1ff322fc014bc14011172f72d
cleftbount <88944951+ForgoneShell@users.noreply.github.com>
2024-06-20 16:29:20 -0500

    upload new website files
    uploaded directly to main directory.

---
+++ scripts/test.js
@@ -0,0 +1,14 @@
+let location1 = "Atai";
+if (location1==="Panselo") {
+    alert("so you are at the beginning...");
+}
+else {
+    alert("you're a decent ways in.");
+}
+
+function multiply(a, b) {
+    let p = a*b;
+    alert(p)
+}
+
+multiply(4, 2)
